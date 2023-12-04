096fa405a752ea9c28fa30e5cc90881c6505d80b
niLPotential <niLPotential@proton.me>
2023-12-04 12:33:41 +0900

    Refactor files

--- main_test.ts
+++ main_test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.208.0/assert/mod.ts";
-import { add } from "./main.ts";
+import { add } from "./main.js";
 
 Deno.test(function addTest() {
   assertEquals(add(2, 3), 5);
