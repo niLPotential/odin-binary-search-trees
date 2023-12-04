469ec6f14a55c00ab7e00dcb4e1c720ae00a79fa
niLPotential <niLPotential@proton.me>
2023-12-04 14:22:05 +0900

    Add buildTree

--- main.js
+++ main.js
@@ -1,8 +1,55 @@
-export function add(a, b) {
-  return a + b;
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
 }
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+export class Tree {
+  constructor(arr) {
+    this.root = buildTree(arr);
+  }
+}
+
+function buildTree(arr) {
+  if (arr.length === 0) {
+    return null;
+  }
+  if (!checkSorted(arr)) {
+    arr.sort((a, b) => a - b);
+    arr = arr.filter(checkDuplicate);
+    console.log(arr);
+  }
+
+  const mid = Math.floor(arr.length / 2);
+  const root = new Node(arr[mid]);
+
+  root.left = buildTree(arr.slice(0, mid));
+  root.right = buildTree(arr.slice(mid + 1));
+
+  return root;
+}
+
+function checkDuplicate(value, index, array) {
+  return index === 0 || value !== array[index - 1];
+}
+
+function checkSorted(arr) {
+  return arr.every(
+    (value, index, array) => index === 0 || value > array[index - 1]
+  );
+}
+
+export function prettyPrint(node, prefix = "", isLeft = true) {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
 }
--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,59 @@
 import { assertEquals } from "https://deno.land/std@0.208.0/assert/mod.ts";
-import { add } from "./main.js";
+import { Tree, prettyPrint } from "./main.js";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+Deno.test(function buildEmptyTreeTest() {
+  const arr: number[] = [];
+  const tree = new Tree(arr);
+  assertEquals(tree.root, null);
+  assertEquals(prettyPrint(tree.root), undefined);
+});
+
+Deno.test(function singleNodeTreeTest() {
+  const arr = [0];
+  const tree = new Tree(arr);
+
+  assertEquals(tree.root?.data, 0);
+  assertEquals(tree.root?.left, null);
+  assertEquals(tree.root?.right, null);
+});
+
+Deno.test(function checkDuplicateTest() {
+  const arr = [0, 0];
+  const tree = new Tree(arr);
+
+  assertEquals(tree.root?.data, 0);
+  assertEquals(tree.root?.left, null);
+  assertEquals(tree.root?.right, null);
+});
+
+Deno.test(function twoNodesTest() {
+  const arr = [0, 1];
+  const tree = new Tree(arr);
+
+  assertEquals(tree.root?.data, 1);
+  assertEquals(tree.root?.left.data, 0);
+  assertEquals(tree.root?.right, null);
+});
+
+Deno.test(function unorderdTest() {
+  const arr = [4, 3, 2, 1, 0];
+  const tree = new Tree(arr);
+
+  assertEquals(tree.root?.data, 2);
+  assertEquals(tree.root?.left.data, 1);
+  assertEquals(tree.root?.left.right, null);
+  assertEquals(tree.root?.left.left.data, 0);
+  assertEquals(tree.root?.left.left.right, null);
+
+  assertEquals(tree.root?.right.data, 4);
+  assertEquals(tree.root?.right.right, null);
+  assertEquals(tree.root?.right.left.data, 3);
+  assertEquals(tree.root?.right.left.right, null);
+});
+
+Deno.test(function longArrTest() {
+  const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+  const tree = new Tree(arr);
+
+  prettyPrint(tree.root);
 });
