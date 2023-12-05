788888222e61925a4b1478b129399c81d0e766fd
niLPotential <niLPotential@proton.me>
2023-12-05 09:07:33 +0900

    Change buildTree to class method

--- Tree.ts
+++ Tree.ts
@@ -5,7 +5,25 @@ export class Tree {
   root: Node | null;
 
   constructor(arr: number[]) {
-    this.root = buildTree(arr);
+    this.root = this.buildTree(arr);
+  }
+
+  buildTree(arr: number[]) {
+    if (arr.length === 0) {
+      return null;
+    }
+    if (!checkSorted(arr)) {
+      arr.sort((a, b) => a - b);
+      arr = arr.filter(checkDuplicate);
+    }
+
+    const mid = Math.floor(arr.length / 2);
+    const root = new Node(arr[mid]);
+
+    root.left = this.buildTree(arr.slice(0, mid));
+    root.right = this.buildTree(arr.slice(mid + 1));
+
+    return root;
   }
 
   insert(value: number) {
@@ -100,22 +118,4 @@ export class Tree {
       }
     }
   }
-}
-
-function buildTree(arr: number[]) {
-  if (arr.length === 0) {
-    return null;
-  }
-  if (!checkSorted(arr)) {
-    arr.sort((a, b) => a - b);
-    arr = arr.filter(checkDuplicate);
-  }
-
-  const mid = Math.floor(arr.length / 2);
-  const root = new Node(arr[mid]);
-
-  root.left = buildTree(arr.slice(0, mid));
-  root.right = buildTree(arr.slice(mid + 1));
-
-  return root;
 }
